8a6321d81aaac831201f9104af6501434c3cbbc6
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-05-03 10:14:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,7 @@ class Config(object):
 
     API_HASH = os.environ.get("API_HASH", "d47c74c8a596fd3048955b322304109d")    
     
-    CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "<b><a href="https://t.me/Spideyofficial_777">{file_name}</a>
-
-🗂️ ꜱɪᴢᴇ : {file_size}
-
-<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ :  <a href='https://telegram.me/Hacker_x_official_777'>ʜᴀᴄᴋᴇʀ_x_ᴏꜰꜰɪᴄɪᴀʟ_777</a></blockquote></b>")
+    CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "<b><a href="https://t.me/Spideyofficial_777">{file_name}</a>\n\n🗂️ ꜱɪᴢᴇ : {file_size}\n\n<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ :  <a href='https://telegram.me/Hacker_x_official_777'>ʜᴀᴄᴋᴇʀ_x_ᴏꜰꜰɪᴄɪᴀʟ_777</a></blockquote></b>")
 
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
 
